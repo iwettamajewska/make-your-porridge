78c1de25536c875efa2a17bf6deaa57403bb9fc1
iwetta <iwetta.majewska@gmail.com>
2022-11-19 10:02:58 +0100

    changes

--- make-your-porridge/src/components/NutritionTable.js
+++ make-your-porridge/src/components/NutritionTable.js
@@ -1,3 +1,5 @@
+import { constantIngredient } from "./data/ingredients";
+
 const measures = {
   calories: "kcal",
   protein: "g",
@@ -6,45 +8,57 @@ const measures = {
   fiber: "g",
   vitaminC: "mg",
   calcium: "mg",
-  pottasium: "mg",
+  pottassium: "mg",
 };
 
 const testText = <p>test test test</p>;
 
 const NutritionTable = ({ nutritions }) => {
   const nutritionsKeys = Object.keys(nutritions);
+  console.log(nutritions);
+
+  const showTable =
+    JSON.stringify(nutritions) !== JSON.stringify(constantIngredient) &&
+    nutritionsKeys.length !== 0;
 
   return (
-    <div className="container-nutrition-data">
-      <h3 className="your-porridge-contains-text">your porridge contains:</h3>
-      <table className="nutrition-table">
-        <thead>
-          <tr className="nutrition-name-section">
-            {nutritionsKeys.map((nutrition, index) => {
-              return (
-                <th key={index} className="nutrition-name">
-                  {nutrition === "vitaminC"
-                    ? "Vitamin C"
-                    : nutrition.charAt(0).toUpperCase() + nutrition.slice(1)}
-                </th>
-              );
-            })}
-          </tr>
-        </thead>
-        <tbody>
-          <tr className="nutrition-data-section">
-            {nutritionsKeys.map((nutrition, index) => {
-              return (
-                <td
-                  key={index}
-                  className="nutrition-data"
-                >{`${nutritions[nutrition]} ${measures[nutrition]}`}</td>
-              );
-            })}
-          </tr>
-        </tbody>
-      </table>
-    </div>
+    <>
+      {showTable && (
+        <div className="container-nutrition-data">
+          <h3 className="your-porridge-contains-text">
+            your porridge contains:
+          </h3>
+          <table className="nutrition-table">
+            <thead>
+              <tr className="nutrition-name-section">
+                {nutritionsKeys.map((nutrition, index) => {
+                  return (
+                    <th key={index} className="nutrition-name">
+                      {nutrition === "vitaminC"
+                        ? "Vitamin C"
+                        : nutrition.charAt(0).toUpperCase() +
+                          nutrition.slice(1)}
+                    </th>
+                  );
+                })}
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="nutrition-data-section">
+                {nutritionsKeys.map((nutrition, index) => {
+                  return (
+                    <td
+                      key={index}
+                      className="nutrition-data"
+                    >{`${nutritions[nutrition]} ${measures[nutrition]}`}</td>
+                  );
+                })}
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      )}
+    </>
   );
 };
 
